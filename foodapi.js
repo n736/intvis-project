d01b59ecef537f25f6986d2e4ce5d74413d6eed8
Nicholas Chite <nickman736@gmail.com>
2021-08-11 17:27:14 -0400

    format 2 spaces

--- foodapi.js
+++ foodapi.js
@@ -1,7 +1,7 @@
 const https = require('https');
 
 module.exports = {
-    getMenuItem
+  getMenuItem
 }
 
 const def_hostname = 'api.spoonacular.com';
@@ -9,24 +9,24 @@ const def_port = 443;
 const id = 424571;
 
 function getMenuItem(id) {
-    let result;
-    const options = {
-        hostname: def_hostname,
-        port: def_port,
-        path: '/food/menuItems/' + id,
-        method: 'GET'
-    }
-    const request = https.request(options, response => {
-        console.log(`statusCode: ${response.statusCode}`);
-        response.on('data', data => {
-            process.stdout.write(data);
-            result = data;
-        })
+  let result;
+  const options = {
+    hostname: def_hostname,
+    port: def_port,
+    path: '/food/menuItems/' + id,
+    method: 'GET'
+  }
+  const request = https.request(options, response => {
+    console.log(`statusCode: ${response.statusCode}`);
+    response.on('data', data => {
+      process.stdout.write(data);
+      result = data;
     })
-    request.on('error', error => {
-        console.error(error);
-    })
-    request.end();
+  })
+  request.on('error', error => {
+    console.error(error);
+  })
+  request.end();
 
-    return result;
+  return result;
 }
