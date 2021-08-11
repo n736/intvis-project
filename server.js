6869815f7e02c0ffa37759433a6e35cfde36f502
Nicholas Chite <nickman736@gmail.com>
2021-08-11 17:27:31 -0400

    format 2 spaces

--- server.js
+++ server.js
@@ -6,22 +6,22 @@ const hostname = '127.0.0.1'
 const port = 80;
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    fs.readFile('./index.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            response.write('Page not found');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    })
+  response.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+  fs.readFile('./index.html', null, function (error, data) {
+    if (error) {
+      response.writeHead(404);
+      response.write('Page not found');
+    } else {
+      response.write(data);
+    }
+    response.end();
+  })
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 
