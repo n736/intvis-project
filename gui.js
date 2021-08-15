adead5b3782956880558ca983a2ab5904733c4d7
Nicholas Chite <nickman736@gmail.com>
2021-08-15 17:17:01 -0400

    some pie charts wow

--- gui.js
+++ gui.js
@@ -9,11 +9,26 @@ const margin = {
 
 const id = 409777;
 
-console.log(foodapi.getURL(id));
-d3.json(foodapi.getURL(id), function (data) {
+d3.json(getURL(id), function (data) {
 
   console.log(data);
+
+  var svg = d3.select("svg");
+
+  const nutrition = data["nutrition"];
+  piechart("Calories", nutrition["calories"], 2000, 100, 100);
+  piechart("Fats", gramsToInt(nutrition["fat"]), 75, 300, 300);
+  piechart("Proteins", gramsToInt(nutrition["protein"]), 100, 500, 500);
+  piechart("Sugar", gramsToInt(nutrition["nutrients"]["Sugar"]), 40, 700, 700);
+  piechart("Carbs", gramsToInt(nutrition["carbs"]), 2000, 900, 900);
+
+
+
 
 
 
 })
+
+function gramsToInt(grams) {
+  return Number(grams.substr(0, grams.length - 2));
+}
--- pie_chart.js
+++ pie_chart.js
@@ -1,64 +1,64 @@
-function piechart(type, food_value, daily_value) {
-	// Amount in the food goes here
-	let food = food_value;
-	//Amount per day goes here
-	let per_day = daily_value;
-	//Ensure our Pie Chart looks right
-	let excess = per_day - food;
+function piechart(type, food_value, daily_value, cx, cy) {
+  // Amount in the food goes here
+  let food = food_value;
+  //Amount per day goes here
+  let per_day = daily_value;
+  //Ensure our Pie Chart looks right
+  let excess = per_day - food;
 
-	var data = [food, excess];
+  var data = [food, excess];
 
-	var svg = d3.select("svg"),
-		width = 200,
-		height = 200,
-		radius = Math.min(width, height) / 2,
-		g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+  var svg = d3.select("svg"),
+    width = 200,
+    height = 200,
+    radius = Math.min(width, height) / 2,
+    g = svg.append("g").attr("transform", "translate(" + cx + "," + cy + ")");
 
 
-	/* Common Colors For Food Types
-	Calories : #5C4EB1
-	Carbohydrates : '#5C4EB1'
-	Proteins : #D04125
-	Fats : #DA8D2A
-	Sugars : #D7DF26
-	Background : #DED9D3 */
-	let primary = '#5C4EB1';
-	if(type == "Carbs") {
-		primary = '#5C4EB1'
-	} else if(type == "Proteins") {
-		primary = '#D04125';
-	} else if(type == "Fats") {
-		primary = '#DA8D2A';
-	} else if(type == "Sugars") {
-		primary = '#D7DF26';
-	}
+  /* Common Colors For Food Types
+  Calories : #5C4EB1
+  Carbohydrates : '#5C4EB1'
+  Proteins : #D04125
+  Fats : #DA8D2A
+  Sugars : #D7DF26
+  Background : #DED9D3 */
+  let primary = '#5C4EB1';
+  if (type == "Carbs") {
+    primary = '#5C4EB1'
+  } else if (type == "Proteins") {
+    primary = '#D04125';
+  } else if (type == "Fats") {
+    primary = '#DA8D2A';
+  } else if (type == "Sugars") {
+    primary = '#D7DF26';
+  }
 
-	var color = d3.scaleOrdinal([primary, '#DED9D3']);
+  var color = d3.scaleOrdinal([primary, '#DED9D3']);
 
-	// Generate the pie
-	var pie = d3.pie();
+  // Generate the pie
+  var pie = d3.pie();
 
-	// Generate the arcs
-	var arc = d3.arc()
-				.innerRadius(0)
-				.outerRadius(radius);
+  // Generate the arcs
+  var arc = d3.arc()
+    .innerRadius(0)
+    .outerRadius(radius);
 
-	//Generate groups
-	var arcs = g.selectAll("arc")
-				.data(pie(data))
-				.enter()
-				.append("g")
-				.attr("class", "arc")
+  //Generate groups
+  var arcs = g.selectAll("arc")
+    .data(pie(data))
+    .enter()
+    .append("g")
+    .attr("class", "arc")
 
-	//Draw arc paths
-	arcs.append("path")
-		.attr("fill", function(d, i) {
-			return color(i);
-		})
-		.attr("d", arc);
+  //Draw arc paths
+  arcs.append("path")
+    .attr("fill", function (d, i) {
+      return color(i);
+    })
+    .attr("d", arc);
 
-	//Print the percentage value under the pie chart
-	
+  //Print the percentage value under the pie chart
 
-	return 0;
+
+  return 0;
 }
