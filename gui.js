eff3b21cd930576d92c5dc442818c59dd64c1fd5
James <jleibring@gmail.com>
2021-08-16 17:56:01 -0400

    ..

--- gui.js
+++ gui.js
@@ -49,8 +49,16 @@ d3.json(getURL(id), function (data) {
   pies.push(piechart("Proteins", gramsToInt(nutrition["protein"]), 100, 625, 515));
   pies.push(piechart("Sugars", getSugar(nutrition["nutrients"]), 40, 1000, 160));
   pies.push(piechart("Carbohydrates", gramsToInt(nutrition["carbs"]), 200, 1000, 515));
+  pies.forEach(pie => {
+    pie.attr("opacity", state == 8 ? 1 : 0)
+  })
 
-var stats = [];
+  var stats = [];
+  stats.push(stat("Calories", nutrition["calories"], 450, 150));
+  stats.push(stat("Fats", gramsToInt(nutrition["fat"]), 450, 200));
+  stats.push(stat("Proteins", gramsToInt(nutrition["protein"]), 450, 250));
+  stats.push(stat("Sugars", getSugar(nutrition["nutrients"]), 750, 250));
+  stats.push(stat("Carbohydrates", gramsToInt(nutrition["carbs"]), 750, 200));
 
 
   var rect = svg.append("rect")
@@ -62,9 +70,9 @@ var stats = [];
       pies.forEach(pie => {
         pie.attr("opacity", state == 8 ? 1 : 0)
       })
-      // texts.forEach(pie => {
-      //   pie.attr("opacity", state == 6 ? 1 : 0)
-      // })
+      stats.forEach(statistic => {
+        statistic.attr("opacity", state == 6 ? 1 : 0)
+      })
     });
 });
 
@@ -82,4 +90,20 @@ function getSugar(nutrients) {
     if (nutrient["name"] == "Sugar") amount = nutrient["amount"];
   })
   return amount;
+}
+
+function stat(type, food_value, cx, cy) {
+  var svg = d3.select("svg"),
+    g = svg.append("g").attr("transform", "translate(" + cx + "," + cy + ")");
+
+  msg = g.append("text")
+  .attr("x", 0)
+  .attr("y", 0)
+  .attr("text-anchor", "front")
+  .attr("font-weight", 800)
+  .attr("font-family", "Arial")
+  .style("font-size", "22px")
+  .text(type + " : " + food_value + (type == "Calories" ? " kcal" : " g"));
+
+  return g;
 }
